5c2cde9c7113ec4fc8d153e17ae7128384ae0bf3
48723247842 <48723247842@protonmail.com>
2020-06-03 04:36:50 -0400

    4

--- vlc_controller/__init__.py
+++ vlc_controller/__init__.py
@@ -283,14 +283,3 @@ class VLCController:
 		except Exception as e:
 			print( e )
 			return False
-
-
-if __name__ == "__main__":
-
-	vlc = VLCController()
-	print( vlc.connected )
-
-	#vlc.add("/media/187A29A07A297B9E/MEDIA_MANAGER/TVShows/TrailerParkBoys/12/Trailer.Park.Boys.S12E02.Godspeed.My.Muscular.Friend.1080p.NF.WEB-DL.DD5.1.H264-SiGMA.mkv")
-	#time.sleep( 3 )
-
-	print( vlc.get_common_info() )
